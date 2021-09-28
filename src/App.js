3573212fcae4a9861ad90df936518f9379505102
soflarity <55975006+soflarity@users.noreply.github.com>
2021-09-28 13:43:35 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import LandingPage from "./pages/LandingPage/LandingPage";
+import './index.css';
+function App() {
+
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
